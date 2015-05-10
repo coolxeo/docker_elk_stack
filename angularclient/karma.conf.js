effe642e89d8453f3562a95e069834d7d3d73694
sloppylopez <slopez@estudioweb130.com>
2015-05-10 23:52:21 +0100

    added angular mocks

--- angularclient/karma.conf.js
+++ angularclient/karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/scripts/*.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'app/scripts/**/*.js',
       'test/**/*Spec.js'
     ],
