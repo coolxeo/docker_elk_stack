961e4f6ad844b02779bf153dff069d0d6c3adc37
sloppylopez <slopez@estudioweb130.com>
2015-05-12 20:14:19 +0100

    checking that docker is connected properly and takes the changes

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -58,7 +58,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({message: 'hooray!'});
 });
 // charge rss from targets.json in docker elastic search
 router.get('/rssload', function(req, res) {
