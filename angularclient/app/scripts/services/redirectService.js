95badb6e25afdfc3fa6e6f23e9a8c80658bef684
sloppylopez <slopez@estudioweb130.com>
2015-05-14 02:14:28 +0100

    fixing code climate to get 4.0

--- angularclient/app/scripts/services/redirectService.js
+++ angularclient/app/scripts/services/redirectService.js
@@ -1,11 +1,11 @@
-'use strict';
-//not used yet, i will use it to take common factor of put the redirection in the controller instead of in the service
-angular.module('angularfireApp')
-  .service('redirectService', [function () {
-    this.redirectTo = function ($location, redirectTo) {
-      if (typeof redirectTo !== 'undefined') {
-        console.log('redirecting to ' + redirectTo);
-        $location.path(redirectTo);
-      }
-    };
-  }]);
+//'use strict';
+////not used yet, i will use it to take common factor of put the redirection in the controller instead of in the service
+//angular.module('angularfireApp')
+//  .service('redirectService', [function () {
+//    this.redirectTo = function ($location, redirectTo) {
+//      if (typeof redirectTo !== 'undefined') {
+//        console.log('redirecting to ' + redirectTo);
+//        $location.path(redirectTo);
+//      }
+//    };
+//  }]);
