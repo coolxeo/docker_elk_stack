a312649cc57293b4882ec210a1a590466d327e1c
sloppylopez <slopez@estudioweb130.com>
2015-05-18 00:35:23 +0100

    added install script

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -21,13 +21,13 @@ sudo npm install -g codeclimate-test-reporter
 sudo npm install -g grunt-cli
 
 # Add a repo where OpenJDK can be found.
-RUN apt-get install -y software-properties-common
-RUN add-apt-repository -y ppa:webupd8team/java
-RUN apt-get update
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
 
 # Auto-accept the Oracle JDK license
-RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-RUN apt-get install -y oracle-java8-installer
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer
 
 echo java -version
 echo docker -version
