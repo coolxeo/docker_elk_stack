aa6ab38fd460bc8c039a80612bbeb65ad476519c
sloppylopez <slopez@estudioweb130.com>
2015-05-18 00:21:48 +0100

    added install script

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -16,6 +16,9 @@ chmod +x /usr/local/bin/docker-compose
 #Code Climate coverage
 sudo npm install -g codeclimate-test-reporter
 
+#Grunt
+sudo npm install -g grunt-cli
+
 # Add a repo where OpenJDK can be found.
 RUN apt-get install -y software-properties-common
 RUN add-apt-repository -y ppa:webupd8team/java
@@ -24,4 +27,9 @@ RUN apt-get update
 # Auto-accept the Oracle JDK license
 RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 RUN apt-get install -y oracle-java8-installer
+
+echo java -version
+echo docker -version
+echo npm -version
+echo node -v
 
