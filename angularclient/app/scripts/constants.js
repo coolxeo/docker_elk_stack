3e11147728174842536057a3d62c68e9a4e8f2b5
sloppylopez <slopez@estudioweb130.com>
2015-05-18 01:10:25 +0100

    added proper text

--- angularclient/app/scripts/constants.js
+++ angularclient/app/scripts/constants.js
@@ -1,4 +1,4 @@
 angular.module('angularfireApp')
-  .constant('FB_USER', '<YOUR_DOCKER_IP>')//firebase_user
+  .constant('FB_USER', '<YOUR_FIRE_BASE_USER>')//firebase_user
   .constant('ES_IP', '<YOUR_EL_IP>')//elastic search ip
   .constant('ES_PORT', '<YOUR_EL_PORT>');//elastic search port
