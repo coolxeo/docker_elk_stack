84ae56c675028e25681b0e59074d75fff2666c67
Sloppy Lopez <slopez@estudioweb130.com>
2015-05-18 20:18:17 +0100

    Update prerequisites_install.sh
    fix for build

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -20,10 +20,10 @@ sudo npm install -g codeclimate-test-reporter
 #Grunt
 sudo npm install -g grunt grunt-cli
 
-# Add a repo where OpenJDK can be found.
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
+# Add a repo where JDK can be found.
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
 
 # Auto-accept the Oracle JDK license
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
