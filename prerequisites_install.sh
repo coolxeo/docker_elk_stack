1c0d992d4fb443835d669f2c0ff30ccbda4bb85d
sloppylopez <slopez@estudioweb130.com>
2015-05-18 20:35:43 +0100

    fix for build

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -8,10 +8,10 @@ sudo apt-get install git
 
 #Docker
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker <YOUR_UBUNTU_USER>
+sudo usermod -aG docker sloppylopez
 
 #Docker-Compose
-curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #Code Climate coverage
