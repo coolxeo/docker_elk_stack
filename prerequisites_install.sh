6fb3af3217fd08fc7056eba7fee164a54c4219d4
Sloppy Lopez <slopez@estudioweb130.com>
2015-05-18 21:53:11 +0100

    Update prerequisites_install.sh
    add proper user

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -8,7 +8,7 @@ sudo apt-get install git
 
 #Docker
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker sloppylopez
+sudo usermod -aG docker <YOUR_UBUNTU_USER>
 
 #Docker-Compose 
 sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
