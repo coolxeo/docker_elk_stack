ab9fb4e06210c55f70a0b333d350cc98955489e8
sloppylopez <slopez@estudioweb130.com>
2015-05-18 21:58:52 +0100

    fix for build

--- prerequisites_install.sh
+++ prerequisites_install.sh
@@ -29,9 +29,6 @@ sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 
-#Init Docker daemon
-sudo docker -d
-
 java -version
 docker --version
 npm --version
