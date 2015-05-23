40d378fbc8363be9847eac63a0417931ff95f97a
sloppylopez <slopez@estudioweb130.com>
2015-05-23 16:11:00 +0100

    chaged deprecated defaultPrevented for preventDefault

--- angularclient/app/scripts/run.js
+++ angularclient/app/scripts/run.js
@@ -8,20 +8,20 @@
 angular.module('angularfireApp')
   .run(['$rootScope', '$location', function ($rootScope, $location) {
     $rootScope.$on('$routeChangeStart', function (event, next) {
-      //console.log('interceptor executing...');
-      //console.log('$rootScope.authData:' + $rootScope.authData + ' next.loginRequired:' + next.loginRequired + ' ' + (typeof $rootScope.authData === 'undefined'));
+      console.log('interceptor executing...');
+      console.log('$rootScope.authData:' + $rootScope.authData + ' next.loginRequired:' + next.loginRequired + ' ' + (typeof $rootScope.authData === 'undefined'));
       if (next.resetCredentials) {
         $rootScope.authData = undefined;
-        //console.log('*$rootScope.authData:' + $rootScope.authData + ' next.loginRequired:' + next.loginRequired + ' ' + (typeof $rootScope.authData === 'undefined'));
+        console.log('*$rootScope.authData:' + $rootScope.authData + ' next.loginRequired:' + next.loginRequired + ' ' + (typeof $rootScope.authData === 'undefined'));
       }
       if (next.loginRequired && typeof $rootScope.authData === 'undefined') {
-        event.preventDefault();
-        //console.log('interceptor redirected to authwithpassword');
+        event.defaultPrevented();
+        console.log('interceptor redirected to authwithpassword');
         $location.path('/authwithpassword');
       } else if (typeof $rootScope.authData !== 'undefined') {
-        //console.log('user logged ' + $rootScope.authData.password.email);
+        console.log('user logged ' + $rootScope.authData.password.email);
       } else {
-        //console.log('authData not required for this view');
+        console.log('authData not required for this view');
       }
     });
   }]);
