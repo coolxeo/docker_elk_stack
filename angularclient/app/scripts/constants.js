4c3a67f1fa87539a8670f5684cf0c3bf4b34289b
sloppylopez <slopez@estudioweb130.com>
2015-05-25 23:13:41 +0100

    bug for incorrect params in firebase

--- angularclient/app/scripts/constants.js
+++ angularclient/app/scripts/constants.js
@@ -2,3 +2,4 @@ angular.module('angularfireApp')
   .constant('FB_USER', 'sloppylopez')//firebase_user
   .constant('ES_IP', '172.17.42.1')//elastic search ip
   .constant('ES_PORT', '9200');//elastic search port.
+
